d9831dc100cfa92659e77e83fbcc7bbe26325002
agustin <agusquiroga648@gmail.com>
2024-07-08 18:46:56 -0300

    cambio texto

--- app/Recursos/page.tsx
+++ app/Recursos/page.tsx
@@ -6,7 +6,7 @@ export default function Recursos() {
             <div className="h-screen bg-gray-100 py-24"  id="Recursos">
                 <h2 className="text-bold text-5xl text-violetaoscuro text-center pt-12 mb-72">Material gratuito</h2>
                 <div className="flex justify-center items-center">
-                    <h4 className="text-violeta text-center text-5xl xl:text-8xl "> PRÓXIMAMENTE </h4>
+                    <h4 className="text-violeta text-center text-2xl xl:text-5xl "> PRÓXIMAMENTE </h4>
                 </div>
             </div>
          </>
