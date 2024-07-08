4584b27fd714fb06383d30bb5a4d36b14a63b693
agustin <agusquiroga648@gmail.com>
2024-07-08 18:53:14 -0300

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ const kalam = Kalam({  weight: ["300", "400", "700"], subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Aires de Pausa",
   description: "Es un espacio de bienestar donde inspiro a las personas a que tomen conciencia para que descubran su poder interior y conecten con su escencia",
+  icons: {
+    icon: '/logo.svg',
+    shortcut: '/logo.svg',
+    apple: '/logo.svg',
+  },
 };
 
 export default function RootLayout({
@@ -17,11 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/logo.svg" /> {/* Ruta a tu imagen favicon */}
-        <title>Aires de Pausa</title> {/* Título de la pestaña del navegador */}
-        <meta name="description" content="Es un espacio de bienestar donde inspiro a las personas a que tomen conciencia para que descubran su poder interior y conecten con su esencia" /> {/* Descripción para SEO */}
-      </Head>
+        <head>
+          <link rel="icon" href="/logo.svg" /> {/* Ruta a tu imagen favicon */}
+          <title>Aires de Pausa</title> {/* Título de la pestaña del navegador */}
+          <meta name="description" content="Es un espacio de bienestar donde inspiro a las personas a que tomen conciencia para que descubran su poder interior y conecten con su esencia" /> {/* Descripción para SEO */}
+        </head>
       <body className={kalam.className}>{children}</body>
     </html>
   );
