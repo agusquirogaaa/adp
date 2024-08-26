02be297f8a403c56e669e16ce8f0b78b25c46b05
agustin <agusquiroga648@gmail.com>
2024-08-25 21:07:07 -0300

    cambios

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,11 +22,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <head>
-          <link rel="icon" href="/hfondovioleta.png" /> {/* Ruta a tu imagen favicon */}
-          <title>Aires de Pausa</title> {/* Título de la pestaña del navegador */}
-          <meta name="description" content="Es un espacio de bienestar donde inspiro a las personas a que tomen conciencia para que descubran su poder interior y conecten con su esencia" /> {/* Descripción para SEO */}
-        </head>
       <body className={kalam.className}>{children}</body>
     </html>
   );
